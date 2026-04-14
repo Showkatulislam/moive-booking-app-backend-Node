e31c0e7fb073fe66a87c824093010a1ddab78233
Showkatulislam <showkatul2@gmail.com>
2026-04-14 22:02:13 +0600

    add authentication and authorization for movie and theatre apis

--- controllers/theatre.controllers.js
+++ controllers/theatre.controllers.js
@@ -1,15 +1,16 @@
 const theatreService = require("../services/theatre.service")
-const {successResponseBody,errorResponseBody} = require("../utils/responsebody")
+const { successResponseBody, errorResponseBody } = require("../utils/responsebody")
+const {STATUS} = require("../utils/constants")
 const create = async (req, res) => {
     try {
         const response = await theatreService.createTheatre(req.body);
         successResponseBody.data = response;
         successResponseBody.message = "Successfully created the theatre."
-        return res.status(201).json(successResponseBody);
+        return res.status(STATUS.CREATED).json(successResponseBody);
     } catch (error) {
         console.log(error)
         errorResponseBody.error = error;
-        return res.status(500).json(errorResponseBody)
+        return res.status(STATUS.INTERNAL_SERVER_ERROR).json(errorResponseBody)
     }
 }
 
@@ -18,11 +19,11 @@ const destroy = async (req, res) => {
         const response = await theatreService.destroy(req.params.id)
         successResponseBody.data = response;
         successResponseBody.message = "Theatre is deleted successfully."
-        return res.status(200).json(successResponseBody)
+        return res.status(STATUS.CREATED).json(successResponseBody)
     } catch (error) {
         errorResponseBody.err = error;
         errorResponseBody.message = error.message
-        return res.status(400).json(errorResponseBody)
+        return res.status(STATUS.INTERNAL_SERVER_ERROR).json(errorResponseBody)
     }
 }
 
@@ -31,11 +32,11 @@ const getTheatreById = async (req,res) => {
         const response = await theatreService.getTheatreById(req.params.id)
         successResponseBody.data = response;
         successResponseBody.message = "Data fetched successfully."
-        return res.status(201).json(successResponseBody)
+        return res.status(STATUS.CREATED).json(successResponseBody)
     } catch (error) {
         errorResponseBody.err = error;
         errorResponseBody.message = error.message;
-        return res.status(500).json(errorResponseBody)
+        return res.status(STATUS.INTERNAL_SERVER_ERROR).json(errorResponseBody)
     }
 }
 
@@ -44,11 +45,11 @@ const fetchAllTheatre = async (req,res) => {
         const response = await theatreService.fetchAllTheatre(req.query)
         successResponseBody.data = response;
         successResponseBody.message = "successfully fatched all the threatres."
-        return res.status(200).json(successResponseBody);
+        return res.status(STATUS.OK).json(successResponseBody);
     } catch (error) {
-        errorResponseBody.error = error;
+        errorResponseBody.err = error;
         errorResponseBody.message = error.message;
-        return res.status(500).json(errorResponseBody)
+        return res.status(STATUS.INTERNAL_SERVER_ERROR).json(errorResponseBody)
     }
 }
 
@@ -59,12 +60,12 @@ const update = async (req,res) => {
         console.log(response)
         successResponseBody.data = response;
         successResponseBody.message = "Successfully updated data."
-        return res.status(200).json(successResponseBody
+        return res.status(STATUS.OK).json(successResponseBody
         );
     } catch (error) {
-        errorResponseBody.error = error;
+        errorResponseBody.err = error;
         errorResponseBody.message = error.message;
-        return res.status(500).json(errorResponseBody);
+        return res.status(STATUS.INTERNAL_SERVER_ERROR).json(errorResponseBody);
     }
 }
 /**
@@ -75,7 +76,6 @@ const update = async (req,res) => {
  */
 const updateMovies = async (req, res) => {
     try {
-        console.log(3333)
         const response = await theatreService.updateMoviesInTheatre(req.params.id, req.body.movieIds, req.body.insert);
         if (response.err) {
             errorResponseBody.err = response.err;
@@ -83,12 +83,12 @@ const updateMovies = async (req, res) => {
         }
         successResponseBody.data = response;
         successResponseBody.message = "successfully updated movies in the theatre.";
-        return res.status(200).json(successResponseBody)
+        return res.status(STATUS.OK).json(successResponseBody)
 
     } catch (error) {
         console.log(error);
         errorResponseBody.err = error;
-        return res.status(500).json(errorResponseBody);
+        return res.status(STATUS.INTERNAL_SERVER_ERROR).json(errorResponseBody);
     }
 }
 
@@ -101,10 +101,10 @@ const getMovies = async (req, res) => {
         }
         successResponseBody.data = response;
         successResponseBody.message = "Successfully fetched the movies for the theatre."
-        return res.status(200).json(successResponseBody)
+        return res.status(STATUS.OK).json(successResponseBody)
     } catch (error) {
         errorResponseBody.err = error;
-        return res.status(500).json(errorResponseBody)
+        return res.status(STATUS.INTERNAL_SERVER_ERROR).json(errorResponseBody)
     }
 }
 
@@ -117,10 +117,10 @@ const checkMovie = async (req, res) => {
         }
         successResponseBody.data = response;
         successResponseBody.message = "successfully checked movie is present in the theatre."
-        return res.status(200).json(successResponseBody)
+        return res.status(STATUS.OK).json(successResponseBody)
     } catch (error) {
         errorResponseBody.err = error;
-        return res.status(500).json(errorResponseBody);
+        return res.status(STATUS.INTERNAL_SERVER_ERROR).json(errorResponseBody);
     }
 }
 
