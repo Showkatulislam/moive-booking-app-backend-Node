f5570e057dd8d4882c9988569ee23eaa9b225aac
Showkatulislam <showkatul2@gmail.com>
2026-04-14 20:52:46 +0600

    Feat: Fixed the hard coded status codes for auth middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,19 +1,19 @@
 const jwt = require("jsonwebtoken");
 const { errorResponseBody } = require("../utils/responsebody");
 const { getUserById } = require("../services/auth.services");
-const { USER_ROLE } = require("../utils/constants");
+const { USER_ROLE,STATUS } = require("../utils/constants");
 
 const isAuthenticated = async (req, res, next) => {
     try {
         const token = req.headers["x-access-token"];
         if (!token) {
             errorResponseBody.err = "No token provided."
-            return res.status(403).json(errorResponseBody)
+            return res.status(STATUS.FORBIDDEN).json(errorResponseBody)
         }
         const response = jwt.verify(token, "saikat");
         if (!response) {
             errorResponseBody.err = "Token not verified"
-            return res.status(404).json(errorResponseBody);
+            return res.status(STATUS.UNAUTHORISED).json(errorResponseBody);
         }
         const user = await getUserById(response.id);
         req.user = user;
@@ -21,10 +21,10 @@ const isAuthenticated = async (req, res, next) => {
     } catch (error) {
         if (error.code == 404) {
             errorResponseBody.err = "User doesn't exist."
-            return res.status(404).json(errorResponseBody)
+            return res.status(STATUS.NOT_FOUND).json(errorResponseBody)
         }
         errorResponseBody.err = error;
-        return res.status(500).json(errorResponseBody)
+        return res.status(STATUS.INTERNAL_SERVER_ERROR).json(errorResponseBody)
     }
 }
 
@@ -32,11 +32,11 @@ const isAdmin = async (req, res, next) => {
     const user = await getUserById(req.user.id);
     if (!user) {
         errorResponseBody.err = "User is not found."
-        return res.status(401).json(errorResponseBody)
+        return res.status(STATUS.NOT_FOUND).json(errorResponseBody)
     }
     if (user.userRole !== USER_ROLE.admin) {
         errorResponseBody.err = "User is not an admin,cannot proceed with the request."
-        return res.status(401).json(errorResponseBody)
+        return res.status(STATUS.BAD_REQUEST).json(errorResponseBody)
     }
     next()
 }
@@ -45,7 +45,7 @@ const isClient = async (req, res, next) => {
     const user = await getUserById(req.user.id);
     if (user.userRole !== USER_ROLE.client) {
         errorResponseBody.err = "User is not an client,cannot proceed with the request."
-        return res.status(401).json(errorResponseBody)
+        return res.status(STATUS.UNAUTHORISED).json(errorResponseBody)
     }
     next()
 }
@@ -53,7 +53,7 @@ const isAdminOrClient = async (req, res, next) => {
     const user = await getUserById(req.user.id);
     if (user.userRole !== USER_ROLE.admin && user.userRole !== USER_ROLE.client) {
         errorResponseBody.err= " User is neither a client not and admin ,cannot procced with the request."
-        return res.status(401).json(errorResponseBody)
+        return res.status(STATUS.UNAUTHORISED).json(errorResponseBody)
     }
     next()
 }
